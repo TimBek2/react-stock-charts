f01920910067364d29ee0df292b25772f15fbbd9
Tim Beck <tim.beck46@gmail.com>
2019-03-25 12:28:46 -0400

    Add chartList component

---
+++ src/chartList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'React';
+
+export default class ChartList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { stockTickers: [] }
+  }
+
+  render() {
+    return (
+      <p>hello there</p>
+    )
+  }
+}
