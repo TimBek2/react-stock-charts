c332be2a79b9cac1173640ae3d538b09e041ea74
Tim Beck <tim.beck46@gmail.com>
2019-03-25 12:10:44 -0400

    Wire up state to ticker form, add handleChange and handleSubmit functions

--- src/stockTickerForm.js
+++ src/stockTickerForm.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 
 export default class StockTickerForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { stockTicker: '' }
+  }
+
   render() {
+    const handleChange = (e) => {
+      this.setState({ stockTicker: e.target.value })
+      console.log('State:', this.state);
+    }
+
+    const handleSubmit = () => {
+      console.log('State on submit:', this.state);
+    }
+
     return (
       <div>
         <h3>Gimme a stock ticker</h3>
@@ -9,9 +23,9 @@ export default class StockTickerForm extends Component {
           If you need help finding a ticker, try looking <a href="https://finance.yahoo.com/lookup/">here</a>
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Ticker Symbol:
-            <input type="text" />
+            <input type="text" onChange={handleChange} />
           </label>
           <input type="submit" value="Enter" />
         </form>
