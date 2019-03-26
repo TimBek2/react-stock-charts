9e72e8ce684bd478143d671c2fee0de2b56cd179
Tim Beck <tim.beck46@gmail.com>
2019-03-25 22:30:26 -0400

    Wire up API fetch and print opening price for first and last time period returned by Alpha Vantage

--- src/StockChart.js
+++ src/StockChart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ALPHA_VANTAGE_API_KEY } from './lib/constants';
+import request from 'axios';
 
 export default class StockChart extends Component {
   static propTypes = {
@@ -18,20 +19,36 @@ export default class StockChart extends Component {
     if (this.props.ticker !== null) {
       const {ticker} = this.props;
       const Api = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${ALPHA_VANTAGE_API_KEY}`
-      fetch(Api, {
-        method: 'GET'
-      }).then(res => {
-          console.log(res)
-        })
-      // this.setState({
-      //   data: stockData
-      // })
+      request.get(Api).then(res => {
+        const fetchedData = {
+          ticker: ticker,
+          // data comes in reverse chronological order, so the earliest is the last
+          first: res.data['Time Series (5min)'][Object.keys(res.data['Time Series (5min)'])[Object.keys(res.data['Time Series (5min)']).length - 1]],
+          last: res.data['Time Series (5min)'][Object.keys(res.data['Time Series (5min)'])[0]]
+        }
+        this.setState({ data: fetchedData });
+        console.log(fetchedData);
+      })
     }
   }
 
   render() {
-    return(
-      <p>{this.props.ticker}</p>
-    )
+    if(this.state.data.ticker) {
+      return (
+        <div>
+          {this.state.data.ticker &&
+            <div className='chart-wrapper'>
+              <p>Ticker: {this.state.data.ticker}</p>
+              <p>First Trade Opening Price: {this.state.data.first['1. open']}</p>
+              <p>Last Trade Opening Price: {this.state.data.last['1. open']}</p>
+            </div>
+          }
+        </div>
+      )
+    } else {
+      return (<p>Nothing here yet</p>)
+    }
+
+
   }
 }
